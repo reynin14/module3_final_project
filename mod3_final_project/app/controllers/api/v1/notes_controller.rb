04df8628e7d45fdfd0511fca0808b44a30c80be0
reynin14 <kyonzon14@gmail.com>
2017-09-30 13:02:14 -0400

    Add files via upload

---
+++ mod3_final_project/app/controllers/api/v1/notes_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::NotesController < ApplicationController
+
+  def index
+    @notes = Note.all
+    render json: @notes, status: 200
+  end
+
+  def create
+    @note = Note.create(note_params)
+    render json: @note, status: 201
+  end
+
+  private
+  def note_params
+    params.permit(:body)
+  end
+
+end
